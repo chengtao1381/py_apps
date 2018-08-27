34c133b1576a33c60b3789624136923f538a1378
chengtao1381 <foronlygeneral@gmail.com>
2018-08-27 17:25:16 +0800

    Create app1.py
    based flask web app

---
+++ app1.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/hello/<username>', method=['GET'])
+def hello(username):
+	return username
+
+if __name__ = '__main__':
+	app.run(host='0.0.0.0', debug=True)
