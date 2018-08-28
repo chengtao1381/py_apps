e29f8eec08a66fd3b27b184fb3c765da55dba9cc
chengtao1381 <foronlygeneral@gmail.com>
2018-08-28 17:02:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ from redis import Redis
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
-@app.route('/')
-def hello():
+@app.route('/<name>', method=['GET'])
+def hello(name):
 	redis.incr('hits')
-	return 'Hello World! I have been seen %s times.' % redis.get('hits')
+	return 'hello %s, I have been seen %s times.' % (name, redis.get('hits'))
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
