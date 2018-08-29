7efb6318b316496585c962e03013d0aa339a870e
chengtao1381 <foronlygeneral@gmail.com>
2018-08-29 14:31:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,15 @@ from redis import Redis
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
-@app.route('/<name>', method=['GET'])
-def hello(name):
-	redis.incr('hits')
-	return 'hello %s, I have been seen %s times.' % (name, redis.get('hits'))
+@app.route('/')
+def hello():
+        redis.incr('hits')
+        return 'Hello World! I have been seen %s times.' % redis.get('hits')
+
+@app.route('/<float:salary>',methods=['get'])
+def salary(salary):
+        print(salary)
+        return '接收浮点型数字参数: %f.' % salary
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', debug=True)
+        app.run(host='0.0.0.0', debug=True)
